97469ba2b501ceec6b2026d2e8536a3595f84f62
Chad Jemmett <chad.jemmett@gmail.com>
2019-04-25 16:24:29 -0600

    Login and sign up gone replaced by welcome when user is signed in

--- foodie-fun/src/components/Header.js
+++ foodie-fun/src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+const thing = localStorage.getItem('token');
+// const thing = null
+console.log("token", localStorage.getItem('token'));
 
 const Header = () => {
   return (
     <div className='header'>
+      {console.log(localStorage.getItem('token'))}
       <div className='logo'>
         <h3>Foodie Fun</h3>
       </div>
 
       <div className='links'>
         <NavLink to='/protected'>My Meals</NavLink>
-        <NavLink to='/register'>Sign Up</NavLink>
-        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/register'>{thing == true ? "Sign Up" : "Welcome"}</NavLink>
+        <NavLink to='/login'>{thing == true ? "Login" : ""}</NavLink>
       </div>
 
 
